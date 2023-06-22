b4a55d0c57d45bdbb521e6c3a2c3f5fdc2bac040
girishzope10 <113250065+girishzope10@users.noreply.github.com>
2023-06-22 19:30:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,13 @@ streamlit.title('My Mom's New Healthy Dinner')
 
 streamlit.header('Breakfast Favorites')
 
-streamlit.text( " 🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text( '🥣 Omega 3 & Blueberry Oatmeal')
 
-streamlit.text(" 🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 
-streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
                
-streamlit.text(' 🥑🍞  Avocado Toast')    
+streamlit.text('🥑🍞 Avocado Toast')    
           
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
    
